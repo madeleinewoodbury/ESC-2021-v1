93fa313e32e9d398c52ccadfb12ff5b4c265f183
madeleinewoodbury <randine25@gmail.com>
2020-03-12 09:22:16 -0500

    Fix issue where NotFound compononet appeared on landing page

--- src/components/layout/NotFound.js
+++ src/components/layout/NotFound.js
@@ -1,22 +1,24 @@
 import React, { Fragment } from 'react';
 
-const NotFound = () => {
+const NotFound = match => {
   return (
-    <Fragment>
-      <div className="notfound background">
-        <div className="banner"></div>
-        <div className="content">
-          <div className="overlay">
-            <div className="container">
-              <h1 className="x-large text-secondary">
-                <i className="fas fa-exclamation-triangle" /> Page Not Found
-              </h1>
-              <p className="lead">Sorry, this page does not exist</p>
+    match.location.pathname !== '/' && (
+      <Fragment>
+        <div className="notfound background">
+          <div className="banner"></div>
+          <div className="content">
+            <div className="overlay">
+              <div className="container">
+                <h1 className="x-large text-secondary">
+                  <i className="fas fa-exclamation-triangle" /> Page Not Found
+                </h1>
+                <p className="lead">Sorry, this page does not exist</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Fragment>
+      </Fragment>
+    )
   );
 };
 
